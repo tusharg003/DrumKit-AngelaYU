b82834fb4ab8f360779cf29e6888ddc989a0679e
tusharg003 <78368640+tusharg003@users.noreply.github.com>
2021-09-25 15:33:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,51 +4,61 @@ for (var i = 0; i < noOfbuttons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
     var buttonN = this.innerHTML;
-    console.log(buttonN);
     makeSound(buttonN);
- 
+    buttonAnimation(buttonN);
   });
 }
 
 document.addEventListener("keydown", function (event) {
 
   makeSound(event.key);
+  buttonAnimation(event.key);
 
 });
 
 function makeSound(key) {
   switch (key) {
     case "w":
-      var audio = new Audio("/sounds/crash.mp3");
+      var audio = new Audio("sounds/crash.mp3");
       audio.play();
       break;
     case "a":
-      var audio = new Audio("/sounds/kick-bass.mp3");
+      var audio = new Audio("sounds/kick-bass.mp3");
       audio.play();
       break;
     case "s":
-      var audio = new Audio("/sounds/snare.mp3");
+      var audio = new Audio("sounds/snare.mp3");
       audio.play();
       break;
     case "d":
-      var audio = new Audio("/sounds/tom-1.mp3");
+      var audio = new Audio("sounds/tom-1.mp3");
       audio.play();
       break;
     case "j":
-      var audio = new Audio("/sounds/tom-2.mp3");
+      var audio = new Audio("sounds/tom-2.mp3");
       audio.play();
       break;
     case "k":
-      var audio = new Audio("/sounds/tom-3.mp3");
+      var audio = new Audio("sounds/tom-3.mp3");
       audio.play();
       break;
     case "l":
-      var audio = new Audio("/sounds/tom-4.mp3");
+      var audio = new Audio("sounds/tom-4.mp3");
       audio.play();
       break;
   }
 }
 
+function buttonAnimation(currentKey){
+
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.toggle("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+
+}
 //  var audio = new Audio('sounds/tom-2.mp3');
 //     audio.play();
 
